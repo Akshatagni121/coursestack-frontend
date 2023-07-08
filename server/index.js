f88f028b716738716aa754e9119b1fef80404b82
Akshat Agnihotri <91088900+Akshatagni121@users.noreply.github.com>
2023-07-08 08:03:00 -0700

    changes

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"https://coursestack.vercel.app/",
+		origin:"https://coursestack.vercel.app",
 		credentials:true,
 	})
 )
